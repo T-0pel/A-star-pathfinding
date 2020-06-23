d8561468c6f7cd5c940c60c229973da5814b8f48
Tomas Oplatek <oplatek.t@gmail.com>
2020-06-23 20:17:40 +0200

    Add Grid and Node classes

--- A-star Custom Implementation/Assets/Scripts/Grid.cs
+++ A-star Custom Implementation/Assets/Scripts/Grid.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    private Node[,] _grid;
+
+    public int GridSizeX;
+    public int GridSizeZ;
+    public int NodeSize;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _grid = new Node[GridSizeX, GridSizeZ];
+        var bottomLeft = new Vector3(transform.position - Vector3.right * (GridSizeX / 2), 0, );
+        for (var x = -GridSizeX; x < GridSizeX; x++)
+        {
+            for (var z = -GridSizeZ; z < GridSizeZ; z++)
+            {
+                _grid[x, z] = new Node { X = x, Z = z };
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_grid != null)
+        {
+            foreach (var node in _grid)
+            {
+                Gizmos.DrawCube(new Vector3(node.X, 0.1f, node.Z), Vector3.one);
+            }
+        }
     }
 }
---
+++ A-star Custom Implementation/Assets/Scripts/Node.cs
@@ -0,0 +1,9 @@
+﻿public class Node
+{
+    public int GCost { get; set; }
+    public int HCost { get; set; }
+    public int FCost => GCost + HCost;
+    public Node Parent { get; set; }
+    public int X { get; set; }
+    public int Z { get; set; }
+}
