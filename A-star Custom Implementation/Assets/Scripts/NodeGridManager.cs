86c5af10f4cc82433942431932945f20bb564284
Tomas Oplatek <oplatek.t@gmail.com>
2020-11-15 08:57:13 +0100

    Add new class for managing a pool of node grids

---
+++ A-star Custom Implementation/Assets/Scripts/NodeGridManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Scripts
+{
+    public class NodeGridManager
+    {
+        private List<Node[,]> _availableNodeGrids = new List<Node[,]>();
+
+        public Node[,] GetNodeGridFromPool()
+        {
+            // TODO: Will this need a lock?
+            if (_availableNodeGrids.Count == 0)
+            {
+                CreateNodeGrid();
+                
+            }
+
+            return _availableNodeGrids.Take(1).First();
+        }
+
+        private void CreateNodeGrid()
+        {
+            
+        }
+    }
+}
